8a93a20ca5603b8685db23e443dd1655af1d1f21
Nikita Shchypylov <shchypylov@gmail.com>
2019-11-06 16:15:36 +0200

    sadfsdfa

--- packages/second/index.js
+++ packages/second/index.js
@@ -1,3 +1,3 @@
-const second = () => 'Hi from the second module you mafaka one more change yo asfdsfasdf fasdfsdfsdf fa sa afd sdfsdfasd ';
+const second = () => 'Hi from the second module you mafaka one more change yo asfdsfasdf fasdfsdfsdf fa sa afd sdfsdfasd asdfsadfs';
 
 module.exports = second;
