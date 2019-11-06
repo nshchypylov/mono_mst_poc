d6644a8717537feb633baef3092c12ae64ceae46
Nikita Shchypylov <shchypylov@gmail.com>
2019-11-06 15:53:33 +0200

    one more commit 1

--- packages/second/index.js
+++ packages/second/index.js
@@ -1,3 +1,3 @@
-const second = () => 'Hi from the second module you mafaka one more change yo';
+const second = () => 'Hi from the second module you mafaka one more change yo asfdsfasdf';
 
 module.exports = second;
