3e2ff1b30d16b116bf80433914b38f9c82c2cc37
Nikita Shchypylov <shchypylov@gmail.com>
2019-11-06 16:17:11 +0200

    my change in second module

--- packages/second/index.js
+++ packages/second/index.js
@@ -1,3 +1,3 @@
-const second = () => 'Hi from the second module you mafaka one more change yo asfdsfasdf fasdfsdfsdf fa sa afd sdfsdfasd asdfsadfs';
+const second = () => 'Just hi mafaka';
 
 module.exports = second;
