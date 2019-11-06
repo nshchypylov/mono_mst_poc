15a44189382a0d473ba0c525073f41b8f3e3f01b
Nikita Shchypylov <shchypylov@gmail.com>
2019-11-06 16:05:20 +0200

    updated lock

--- packages/second/index.js
+++ packages/second/index.js
@@ -1,3 +1,3 @@
-const second = () => 'Hi from the second module you mafaka one more change yo asfdsfasdf fasdfsdfsdf';
+const second = () => 'Hi from the second module you mafaka one more change yo asfdsfasdf fasdfsdfsdf fa sa';
 
 module.exports = second;
